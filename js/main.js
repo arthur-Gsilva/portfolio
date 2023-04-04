848b157af9ebc688cd32b2f47f194cfc1fcd1de5
Arthur Gabriel <arthurgdasilva77@gmail.com>
2023-04-04 08:46:06 -0300

    add react project

--- js/main.js
+++ js/main.js
@@ -55,30 +55,6 @@ window.addEventListener('scroll', scrollActive)
 
 // PROJECTS SLIDER
 
-let projectsHtmlCss = [
-    {title: 'Hallowen', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/halloween/', src: '../assets/images/projects/halloween.png'},
-    {title: 'Projeto Social', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/projeto-social/', src: '../assets/images/projects/social.png'},
-    {title: 'Blackboard', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/blackboard/', src: '../assets/images/projects/blackboard.png'},
-    {title: 'Gym', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/gym/', src: '../assets/images/projects/gym.png'},
-    {title: 'Awax', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/Awax/', src: '../assets/images/projects/awax.png'},
-    {title: 'Riot', href: 'https://arthur-gsilva.github.io/projetos-html-e-css/lol-login/', src: '../assets/images/projects/riot.png'}
-]
-
-let projectsJavascript = [
-    {title: 'Games Room', href: 'https://games-room7gd.netlify.app/', src: '../assets/images/projects/games-room.png'},
-    {title: 'Calculadora IMC', href: 'https://imcalc-calculator.netlify.app/', src: '../assets/images/projects/calculadora-imc.png'},
-    {title: 'Pokédex', href: 'https://arthur-gsilva.github.io/projetos-javascript/pokedex/', src: '../assets/images/projects/pokedex.png'},
-    {title: 'clima', href: 'https://arthur-gsilva.github.io/projetos-javascript/clima/', src: '../assets/images/projects/clima.png'},
-    {title: 'Music Player', href: 'https://arthur-gsilva.github.io/projetos-javascript/music-player/', src: '../assets/images/projects/music-player.png'},
-    {title: 'Validador de form', href: 'https://arthur-gsilva.github.io/projetos-javascript/validador-de-form/', src: '../assets/images/projects/validator.png'},
-    {title: 'Todo List', href: 'https://arthur-gsilva.github.io/projetos-javascript/todo/', src: '../assets/images/projects/todo.png'}
-]
-
-let projectsReact = [
-    {title: 'Calculadora IMC', href: 'https://calculator-imc-two.vercel.app/', src: '../assets/images/projects/calculadora-imc-react.png'},
-    {title: 'Todo List', href: 'https://react-todo-list-nu-seven.vercel.app/', src: '../assets/images/projects/todo-list-react.png'},
-    {title: 'Movies', href: 'https://react-movies-chi.vercel.app/', src: '../assets/images/projects/react-movies.png'}
-]
 
 const swiperHtmlCss = document.querySelector('.swiper-html-css')
 const swiperJavascript = document.querySelector('.swiper-javascript')
